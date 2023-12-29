cb2457520889e6923f4d99b17d8b64d59944fa7e
Tsvetan Vasilev <tsvetan.vasilev@gmail.com>
2023-12-29 17:15:51 +0200

    Added hardcoded communication between microservices without ServiceDiscovery server.

---
+++ customer/src/main/java/com/tsvetanv/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.tsvetanv.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- fraud/src/main/java/com/tsvetanv/fraud/FraudCheckController.java
+++ fraud/src/main/java/com/tsvetanv/fraud/FraudCheckController.java
@@ -1,11 +1,13 @@
 package com.tsvetanv.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckController {
 
     private final FraudCheckService fraudCheckService;
@@ -13,6 +15,7 @@ public class FraudCheckController {
     @GetMapping(path="{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for cutomer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
