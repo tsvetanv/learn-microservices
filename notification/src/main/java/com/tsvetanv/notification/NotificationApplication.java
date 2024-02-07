d4ffc20e3098eabcc8e0775fa0f1130d008842b4
Tsvetan Vasilev <tsvetan.vasilev@gmail.com>
2024-02-07 07:18:59 +0200

    RabbitMQ: Published successfully  messages.

--- notification/src/main/java/com/tsvetanv/notification/NotificationApplication.java
+++ notification/src/main/java/com/tsvetanv/notification/NotificationApplication.java
@@ -1,13 +1,32 @@
 package com.tsvetanv.notification;
 
+import com.tsvetanv.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.tsvetanv.notification",
+                "com.tsvetanv.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig){
+//        return args -> {
+//            producer.publish(new Person("John", 1),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
