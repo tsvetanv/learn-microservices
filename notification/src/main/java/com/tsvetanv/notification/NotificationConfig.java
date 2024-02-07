324b48b403521259d196e5dc3c7377a53200fe26
Tsvetan Vasilev <tsvetan.vasilev@gmail.com>
2024-02-07 06:48:58 +0200

    RabbitMQ: Created beans for exchange, queue and the binding between them.

--- notification/src/main/java/com/tsvetanv/notification/NotificationConfig.java
+++ notification/src/main/java/com/tsvetanv/notification/NotificationConfig.java
@@ -1,6 +1,11 @@
 package com.tsvetanv.notification;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,6 +19,24 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange(){
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue(){
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalNotificationBinding(){
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 
     public String getInternalExchange() {
         return internalExchange;
